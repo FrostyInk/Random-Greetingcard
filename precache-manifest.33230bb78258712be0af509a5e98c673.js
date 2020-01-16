9ab96be14c1d366fd5f9830faef91f72a33f9673
Andrew <Andryha.p@gmail.com>
2020-01-16 22:12:43 +0200

    Updates

---
+++ precache-manifest.33230bb78258712be0af509a5e98c673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25a536584216da31a807e9157019cca",
+    "url": "/Random-Greetingcard/index.html"
+  },
+  {
+    "revision": "b819010eb0c21b92fbc0",
+    "url": "/Random-Greetingcard/static/css/main.15dac1a1.chunk.css"
+  },
+  {
+    "revision": "0b80e8e887585ce82067",
+    "url": "/Random-Greetingcard/static/js/2.b072d2dd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Random-Greetingcard/static/js/2.b072d2dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b819010eb0c21b92fbc0",
+    "url": "/Random-Greetingcard/static/js/main.3699a2f7.chunk.js"
+  },
+  {
+    "revision": "a185a48b38eff68f4fa4",
+    "url": "/Random-Greetingcard/static/js/runtime-main.cb02b9ae.js"
+  }
+]);
